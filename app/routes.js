04f86b630cffd1a61cc338bf3ca1b607c8df1722
Marc Chavarria <marc.a.chavarria3@gmail.com>
2025-11-03 08:23:06 -0500

    Uploading project

--- app/routes.js
+++ app/routes.js
@@ -36,11 +36,26 @@ module.exports = function(app, passport, db) {
       })
     })
 
-    app.put('/messages', (req, res) => {
+    app.put('/messages/thumbup', (req, res) => {
       db.collection('messages')
       .findOneAndUpdate({name: req.body.name, msg: req.body.msg}, {
         $set: {
           thumbUp:req.body.thumbUp + 1
+        }
+      }, {
+        sort: {_id: -1},
+        upsert: true
+      }, (err, result) => {
+        if (err) return res.send(err)
+        res.send(result)
+      })
+    })
+
+    app.put('/messages/thumbdown', (req, res) => {
+      db.collection('messages')
+      .findOneAndUpdate({name: req.body.name, msg: req.body.msg}, {
+        $inc: {
+          thumbUp: -1
         }
       }, {
         sort: {_id: -1},
--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 // config/database.js
 module.exports = {
 
-    'url' : 'mongodb+srv://demo:demo@cluster0.q2ojb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', 
-    'dbName': 'demo'
+    'url' : 'mongodb+srv://marcachavarria3_db_user:LqeisQYtgUFEuBbk@cluster21.bfsz2it.mongodb.net/21-auth?appName=Cluster21', 
+    'dbName': '21-auth'
 };
